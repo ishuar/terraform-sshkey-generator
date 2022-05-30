6ff92c89a85e7ac0b066e07c18b49492baa7efea
Ishan Sharma <ishansharma887@gmail.com>
2022-05-30 02:53:13 +0200

    AZ KV feature to store ppk key as az kv secret

---
+++ key_vault.tf
@@ -0,0 +1,48 @@
+data "azurerm_client_config" "current" {
+}
+resource "azurerm_resource_group" "ssh_key_kv_rg" {
+  count    = var.enable_az_key_vault_secret ? 1 : 0
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+#tfsec:ignore:azure-keyvault-specify-network-acl
+resource "azurerm_key_vault" "password_key_vault" {
+  count                       = var.enable_az_key_vault_secret ? 1 : 0
+  name                        = var.ssh_key_vault_name
+  location                    = azurerm_resource_group.ssh_key_kv_rg[0].location
+  resource_group_name         = azurerm_resource_group.ssh_key_kv_rg[0].name
+  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = var.purge_protection_enabled #tfsec:ignore:azure-keyvault-no-purge
+  sku_name                    = "standard"
+  tags                        = var.tags
+}
+resource "azurerm_key_vault_access_policy" "user_access_policy" {
+  count        = var.enable_az_key_vault_secret ? 1 : 0
+  key_vault_id = azurerm_key_vault.password_key_vault[0].id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Set",
+    "Get",
+    "Delete",
+    "Purge",
+    "Recover",
+    "List",
+  ]
+}
+resource "azurerm_key_vault_secret" "kv_secret" {
+  count           = var.enable_az_key_vault_secret ? 1 : 0
+  name            = var.ssh_kv_secret
+  value           = tls_private_key.ssh.private_key_pem
+  key_vault_id    = azurerm_key_vault.password_key_vault[0].id
+  content_type    = "ssh-key"
+  expiration_date = var.expiration_date
+  tags            = var.tags
+  depends_on = [
+    azurerm_key_vault_access_policy.user_access_policy
+  ]
+}
